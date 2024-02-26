4872f17a4075ce078f182291db4b7e6ddcd7846f
shadhin-int <shadhin.int@gmail.com>
2024-02-26 14:50:14 +0600

    project setup

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"net/http"
+)
+
+func Customer() chi.Router {
+	route := chi.NewRouter()
+
+	return route
+}
+
+func Invoice() chi.Router {
+	route := chi.NewRouter()
+
+	return route
+}
+
+func main() {
+	router := chi.NewRouter()
+
+	router.Mount("customer/", Customer())
+	router.Mount("invoice/", Invoice())
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		return
+	}
+}
